ea56951c94f8a715a04964313d39902f5389a4df
Subham Sarkar <subhamsarkar.aeie2021@nsec.ac.in>
2024-12-02 20:38:47 +0530

    Modified Final version

--- p8.sql
+++ p8.sql
@@ -41,3 +41,77 @@ join customer
 on salesman.city != customer.city
 where  grade is not null;
 
+
+create  table  company(
+
+    cid int primary key ,
+    cname varchar(30)
+);
+
+insert into  company(cid, cname) VALUES
+                                         (11, 'Samsung'),
+    (12, 'iBall'),
+    (13, 'Epsion'),
+    (14, 'Zebronics'),
+    (15, 'Asus'),
+    (16, 'Frontech');
+
+create  table  product(
+
+    pid int primary key ,
+    pname varchar(30),
+    price decimal(10,2),
+    pcom int
+);
+
+insert into  product(pid, pname, price, pcom) VALUES
+                                                      (101, 'Mother Board', 3200.00, 15),
+    (102, 'Key Board', 450.00, 16),
+    (103, 'ZIP drive', 250.00, 14),
+    (104, 'Speaker', 550.00, 16),
+    (105, 'Monitor', 5000.00, 11),
+    (106, 'DVD drive', 900.00, 12),
+    (107, 'CD drive', 800.00, 12),
+    (108, 'Printer', 2600.00, 13),
+    (109, 'Refill cartridge', 350.00, 13),
+    (110, 'Mouse', 250.00, 12);
+
+select  product.pid, product.pname,product.price,company.cname
+from  product
+join company
+on product.pcom=company.cid
+
+
+create  table   company(
+
+    cid int primary key ,
+    cname varchar(30)
+);
+
+insert into  company(cid, cname) VALUES
+                                     (11, 'Samsung'),
+(12, 'iBall'),
+(13, 'Epsion'),
+(14, 'Zebronics'),
+(15, 'Asus'),
+(16, 'Frontech');
+
+create  table   product(
+
+    pid int primary key ,
+    pname varchar(30),
+    price  decimal(10,2),
+    pcom int,
+    foreign key (pcom) references  company(cid)
+);
+
+insert into  product(pid, pname, price, pcom) VALUES
+                                                  (101, 'Mother Board', 3200.00, 15),
+(102, 'Key Board', 450.00, 16),
+(103, 'ZIP drive', 250.00, 14),
+(104, 'Speaker', 550.00, 16);
+
+select  product.pid,product.pname,company.cname,company.cid,product.price
+from product
+join company
+on product.pcom=company.cid
